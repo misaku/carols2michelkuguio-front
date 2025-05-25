a4a695b56fadecd983300a928a9f6e47b8fe003d
Michel dos Santos Kuguio <michel.kuguio@gmail.com>
2025-05-25 12:15:18 -0300

    feat: finish mvc

--- src/App.styles.tsx
+++ src/App.styles.tsx
@@ -23,6 +23,8 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         border: 0;
+        font-size: 100%;
+        font: inherit;
         vertical-align: baseline;
     }
 
