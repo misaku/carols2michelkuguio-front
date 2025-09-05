0e55843d65a2e47edf4de933f9ccb5a085429b0e
Michel dos Santos Kuguio <michel.kuguio@gmail.com>
2025-09-04 23:33:54 -0300

    feat: add galery

--- src/components/Galery/index.tsx
+++ src/components/Galery/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import {useEffect} from "react";
 import LightGallery from 'lightgallery/react';
 
 // import styles
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="vite/client" />
+declare module 'flickr-justified-gallery'
